a53ae512e4886768c683dd4e32fb1cef28d14abe
hitourebito <yuto0423777@gmail.com>
2019-08-29 15:31:18 +0900

    Write print_r

---
+++ ex05_01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>ex05_01.html</title>
+</head>
+<body>
+  <h1>呼び出しによる$_SERVERの違い</h1>
+  <a href="ex05_01.php">リンク呼び出し</a>
+  <pre>
+    <?php 
+      print_r($_SERVER);
+    ?>
+  </pre>
+</body>
