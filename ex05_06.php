3b5d73f62fa8cf1f6a251b2e65ad24e18d4d7ffa
hitourebito <yuto0423777@gmail.com>
2019-09-01 07:39:04 +0900

    set inputtype="checkbox"

--- ex05_06.php
+++ ex05_06.php
@@ -11,4 +11,22 @@
   <?php
     
   ?>
+  <form action="<?= $_SERVER["SCRIPT_NAME"]?>" method="POST">
+    <div>求める値: 
+      <input type="checkbox" name="require_value[]" value="直径">直径
+      <input type="checkbox" name="require_value[]" value="円周">円周
+      <input type="checkbox" name="require_value[]" value="円の面積">円の面積
+      <input type="checkbox" name="require_value[]" value="球の体積">球の体積
+      <input type="checkbox" name="require_value[]" value="球の表面積">球の表面積
+      <input type="checkbox" name="require_value[]" value="直径">直径
+      <input type="hidden" name="count" value="1">
+    </div>
+    <div>
+      半径:<input type="text" name="hankei" value=<?= $_POST["hankei"]?>>
+    </div>
+    <input type="submit" name="btn" value="送信">
+  </form>
+  <?php
+
+  ?>
 </body>
