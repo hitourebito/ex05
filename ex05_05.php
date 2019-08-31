3cd167cf5733142c3979e6a2632556b8fc1a596a
hitourebito <yuto0423777@gmail.com>
2019-08-31 23:31:28 +0900

    enzansi button

--- ex05_05.php
+++ ex05_05.php
@@ -41,20 +41,23 @@
     }
   ?>
 
-<form action="ex04_04.php" method="POST">
+<form action="<?= $_SERVER["SCRIPT_NAME"]?>" method="POST">
   <input type="text" name="number1" value="<?php echo $num1 ?>">
-  <input type="text" name="enzansi" value="<?php echo $enzan ?>">
-  <input type="text" name="number2" value="<?php echo $num2 ?>">
+  <input type="text" name="number2" value="<?php echo $num2 ?>"><br/>
+  <input type="hidden" name="count" value="1">
+  <input type="submit" name="btn" value="+">
+  <input type="submit" name="btn" value="-">
+  <input type="submit" name="btn" value="*">
+  <input type="submit" name="btn" value="*">
+  <input type="submit" name="btn" value="%">
     <?php if($errflg === 0) {
       echo "=", $sum;
     } ?><br/>
-  <input type="submit" name="btn" value="送信">
 </form>
 
   <?php
-    if($errflg !== 0) {
+    if($errflg !== 0 && $_POST["count"] === "1") {
       echo "正しく入力してください";
     } 
-  ?>
   ?>
 </body>
