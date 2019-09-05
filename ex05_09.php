abb4b9648c53d4282809941447ee422ec9260ebc
hitourebito <yuto0423777@gmail.com>
2019-09-05 14:00:50 +0900

    add value and array

--- ex05_09.php
+++ ex05_09.php
@@ -1,6 +1,6 @@
 <?php //php1ここから
-
-
+  $station_tbl = array("中百舌鳥", "深井", "泉ヶ丘", "栂・美木多", "光明池", "和泉中央");
+  $station_judge = array_fill(0, 6, 0);
 //php1ここまで ?>
 
 <!DOCTYPE html>
@@ -14,7 +14,12 @@
 </head>
 <body>
   <form action="<?= $_SERVER["SCRIPT_NAME"]?>" method="POST">
+    <select name="station[]" size="6" multiple="multiple">
+    <?php//php2ここから
+      
+    //php2ここまで?>
 
+    </select>
   </form>
 <?php //php2ここから
 
