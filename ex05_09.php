22b6be3b92fefc5f4f5fcac2d6bfdf81f15596e9
hitourebito <yuto0423777@gmail.com>
2019-09-05 14:17:12 +0900

    add $errmsg and $errflg

--- ex05_09.php
+++ ex05_09.php
@@ -1,16 +1,25 @@
 <?php //php1ここから
   $station_tbl = array("中百舌鳥", "深井", "泉ヶ丘", "栂・美木多", "光明池", "和泉中央");
+  $errmsg = array();
   $station_judge = array_fill(0, 6, 0);
   $pflg = 0;
+  $errflg = 0;
+
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pflg = 1;
+
+    if (isset($_POST["station"]) == FALSE || count($_POST["station"]) === 1) {
+      $errflg += 1;
+      $errmsg[] = "駅を2つ以上選択してください";
+    }
 
     if (isset($_POST["station"]) && count($_POST["station"])) {
       foreach ($_POST["station"] as $value) {
         $station_judge[$value] = 1;
       }
     }
+
   }
 //php1ここまで ?>
 
@@ -34,9 +43,11 @@
         }
         echo ">$value</option>";
       }
+      echo "<br/>";
     //php2ここまで ?>
-
     </select>
+    <br/>
+    <input type="submit" name="btn" value="送信">
   </form>
 <?php //php2ここから
 
